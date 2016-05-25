0a75f841502fff6f44862b7477d933f0931b4d5a
Tim Radtke <tjradtke@mtu.edu>
2016-05-25 15:45:36 -0400

    Initial Commit

---
+++ assets/js/default.js
@@ -0,0 +1,5 @@
+
+// Append "Javascript working" to the web page to show that the default.js file is linked correctly.
+setTimeout(function() {
+	$('p').append("<p>Javascript working</p>");
+},1500);
